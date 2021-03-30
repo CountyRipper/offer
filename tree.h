5a8692f0915534dbf417a1aec4129f50700f7283
Dai <1097249217@qq.com>
2021-03-30 21:02:42 +0800

    链表树，三种递归遍历

---
+++ tree.h
@@ -0,0 +1,83 @@
+#include <vector>
+#include <iostream>
+//父子结点单项结点树的链表实现
+using namespace std;
+template <class E>
+class node
+{
+public:
+    E element;
+    node *left;
+    node *right;
+    node(E ele, node *l = nullptr, node *r = nullptr)
+    {
+        this->element = ele;
+        this->left = l;
+        this->right = r;
+    }
+    node();
+    void print()
+    {
+        cout << element << " ";
+    }
+};
+
+template <class E>
+class tree
+{
+public:
+    node<E> *root;
+    tree(node<E> *root)
+    {
+        this->root = root;
+    }
+};
+//前序遍历(递归)
+//先访问根节点，然后访问左节点，最后访问右节点
+template <class E>
+void pretravelre(node<E>* t)
+{
+    node<E> *cur = t;
+    //visit
+    cur->print();
+    if (cur->left != nullptr)
+    {
+        pretravelre(cur->left);
+    }
+    //左边没有了
+    if (cur->right != nullptr)
+    {
+        pretravelre(cur->right);
+    }
+}
+//中序遍历(递归)
+template <class E>
+void midtravelre(node<E>* t)
+{
+    node<E> *cur = t;
+    if (cur->left != nullptr)
+    {
+        midtravelre(cur->left);
+    }
+    cur->print();
+    if (cur->right != nullptr)
+    {
+        midtravelre(cur->right);
+    }
+}
+//后续遍历(递归)
+template <class E>
+void backtravelre(node<E>* t)
+{
+    node<E> *cur = t;
+    if (cur->left != nullptr)
+    {
+        midtravelre(cur->left);
+    }
+
+    if (cur->right != nullptr)
+    {
+        midtravelre(cur->right);
+    }
+    cur->print();
+}
