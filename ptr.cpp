4422e888e341f6f6142132be91e820b945e3fc5f
Maverick <1097249217@qq.com>
2021-07-27 12:23:55 +0800

    测试

---
+++ ptr.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int *arr = new int[10];
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = i;
+    }
+    cout<<arr<<endl;
+    cout<<*arr<<endl;
+    
+}
