e57dd3e4bbb0a77e5d512e0d696329a684b334ac
Maverick <1097249217@qq.com>
2021-09-08 11:30:52 +0800

    优化调整函数

--- heap-sort.cpp
+++ heap-sort.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-void adjustheap(vector<int>& a, int start)
+void adjustheap(vector<int>& a, int end)
 {
-    int end = a.size() - 1;
-    for (int i = 0; i <= end - start - 1; i++)
+    //int end = a.size() - 1;
+    for (int i = end; i > 0; i--)
     {
-        int tmp = (end - start - i); //当前元素位置
-        if (a[tmp] > a[(tmp - 1) / 2])
+
+        if (a[i] > a[(i - 1) / 2])
         {
-            swap(a[tmp], a[(tmp - 1) / 2]);
+            swap(a[i], a[(i - 1) / 2]);
         }
     }
     //调整完毕
-    swap(a[0], a[end - start]); //交换最大堆堆顶元素到叶子
+    swap(a[0], a[end]); //交换最大堆堆顶元素到叶子
 }
-void heap_sort(vector<int>& a, int start)
+void heap_sort(vector<int>& a, int end)
 {
-    adjustheap(a, start);
-    if (start < a.size() - 1)
+    adjustheap(a, end);
+    if (end > 0)
     {
-        heap_sort(a, start + 1);
+        heap_sort(a, end - 1);
     }
 }
 int main()
 {
     vector<int> a = { 5, 4, 3, 6, 54, 7, 2, 1, 3, 55, 8 };
-    heap_sort(a, 0);
+    heap_sort(a, a.size() - 1);
     for (auto&& i : a)
     {
         cout << i << ",";
