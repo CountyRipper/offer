a42eb44e3022ad53966034feca9f8ad1b6019ded
dai <1097249217>
2022-03-11 17:45:42 +0800

    前缀和

---
+++ acwing/795_presum.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int N = 100001;
+int main() {
+    int a[N];
+    int s[N + 1];
+    int n, m;
+    ios::sync_with_stdio(false);
+    cin >> n >> m;
+    for (int i = 0;i < n;i++) {
+        cin >> a[i];
+    }
+    s[0] = 0;
+    for (int i = 0; i < n;i++) {
+        s[i + 1] = s[i] + a[i];
+    }
+    for (int i = 0; i < m; i++) {
+        int j, k;
+        cin >> j >> k;
+        cout << s[k] - s[j - 1] << endl;
+    }
+}
