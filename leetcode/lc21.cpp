49498957b4b70bbb4afcb12489e5847720f13aed
dai-r9000p <1097249217@qq.com>
2022-04-06 10:13:52 +0800

    merge order linklist

--- leetcode/lc21.cpp
+++ leetcode/lc21.cpp
@@ -40,12 +40,30 @@ struct ListNode {
         }
         return ml;
     }
+ListNode* mergeTwoLists_1(ListNode* l1, ListNode* l2){
+    if(l1==nullptr&& l2==nullptr) return nullptr;
+    ListNode* head = new ListNode(INT32_MIN);
+    ListNode* prev = head;
+    while(l1!=nullptr&&l2!=nullptr){
+        if(l1->val>l2->val){
+            prev->next=l2;
+            l2=l2->next;
+        }
+        else{
+            prev->next=l1;
+            l1=l1->next;
+        }
+        prev=prev->next;
+    }
+    prev->next= l1==nullptr?l2:l1;
+    return head->next;
+}
 int main(){
     ListNode* l1=new ListNode(2);
     //l1->next=new ListNode(2);
     ListNode* l2= new ListNode(1);
     //l2->next=new ListNode(3);
-    ListNode* t=mergeTwoLists(l1,l2);
+    ListNode* t=mergeTwoLists_1(l1,l2);
     while(t!=nullptr){
         cout<<t->val<<" ";
         t=t->next;
