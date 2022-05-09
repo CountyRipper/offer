70ebf5ebb51dc306252a4399b1c6e6418c6cf2ae
dai-r9000p <1097249217@qq.com>
2022-05-09 23:34:17 +0900

    加油站，贪心+局部最优

---
+++ leetcode/lc134.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+    int cursum=0;
+    int start = 0;
+    for (int i = 0; i < gas.size(); i++)
+    {
+        cursum += gas[i]-cost[i];
+        if(cursum<0){
+            cursum = 0;
+            start = i+1;
+        }
+    }
+    if(cursum<0) return -1;
+    return start;
+}
